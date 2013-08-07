ea14e945d24dafbf68ea9f6e30528c75178467d4
Dani <dkatz6@binghamton.edu>
2013-08-06 22:05:50 -0400

    Create testGraph.java

---
+++ testGraph.java
@@ -0,0 +1,15 @@
+
+package graphproj;
+
+public class testGraph {
+
+  public static void main(String[]args){
+	
+		
+		Graph g1 = new Graph();
+		g1.createGraph();
+		System.out.println(g1);
+	}
+
+	
+}
